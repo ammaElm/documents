ae22c1b664ca1ef401d7ec6635d6ce14b2e267f5
zhangminyan <zhangminyan@sensetime.com>
2022-01-17 20:04:14 +0800

    feat: title

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports =  {
+  title: 'Components',
+  description: 'My Components ~',
+  head: [
+    ['link', { rel: 'icon', href: '/favicon.ico' }] // 增加一个自定义的 favicon(网页标签的图标)
+  ],
+  base: '/', // 这是部署到github相关的配置 下面会讲
+  port: 8864,
+  hot: true,
+
+}
