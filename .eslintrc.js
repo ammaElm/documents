2671f2cbe7277c268ebfe324aec4d6fa2bfe7a86
zhangminyan <zhangminyan@sensetime.com>
2022-02-15 17:22:35 +0800

    fix:eslint emit prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,9 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:vue/vue3-recommended',
-    "prettier"
-  ],
+  extends: ["eslint:recommended", "prettier", "plugin:prettier/recommended"],
   rules: {
     // override/add rules settings here, such as:
     // 'vue/no-unused-vars': 'error'
-  }
-}
+  },
+};
